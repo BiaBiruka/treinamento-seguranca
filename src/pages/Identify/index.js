4f78326347294db6524cf409f5a7cf2267327f80
Beatriz Parolin <beatrizparolin@gmail.com>
2024-08-27 13:36:06 -0300

    feat: identify

--- src/pages/Identify/index.js
+++ src/pages/Identify/index.js
@@ -1,10 +1,44 @@
 import React from "react";
 import NextPage from "../../Components/NextPage";
+import Folders from "../../assets/folders.jpg";
+import Carlos from "../../assets/carlos.jpg";
 
 const Identify = () => {
   return (
     <div>
-      Identify
+      <div className="identify-first-section">
+        <div className="section-content">
+          <h1>Atenção é proteção</h1>
+          Apesar de não ser uma tarefa fácil, muitas vezes é possível perceber
+          alterações em um arquivo observando suas características. Se você se
+          deparar com uma mídia ou documento que ache suspeitos, tente
+          perceber...
+          <ul>
+            <li>Se o arquivo é anormalmente pesado;</li>
+            <li>Se a frequência do áudio possui alterações;</li>
+            <li>Se a mídia possui cores estranhas em alguns pontos;</li>
+          </ul>
+          Caso possua uma versão original do arquivo que recebeu, tente{" "}
+          <b>comparar</b> ambos para ver se algo parece certo.
+        </div>
+        <img src={Folders} alt="Fileira de arquivos" />
+      </div>
+      <div className="identify-second-section">
+        <img src={Carlos} alt="Fileira de arquivos" />
+
+        <div className="section-content">
+          <h1>O que você faria?</h1>
+          <span>
+            Carlos trabalha no time de Segurança da Informação, sendo o
+            principal responsável pela análise das comunicações e arquivos da
+            empresa para garantir que não haja vazamento de informações.
+            Recentemente Carlos percebeu que alguns arquivos de imagem estão
+            parecendo suspeitos, e resolveu investigar mais a fundo. Quais dos
+            arquivos abaixo parecem mais suspeitos?
+            <i>Clique nos cartões para ver a resposta.</i>
+          </span>
+        </div>
+      </div>
       <NextPage />
     </div>
   );
